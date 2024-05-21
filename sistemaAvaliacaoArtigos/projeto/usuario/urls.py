2703cd01976c7836fbf99e461e68c0e53e29e810
Alexandre de O. Zamberlan <alexz@ufn.edu.br>
2024-05-21 16:54:10 -0300

    Update urls.py

--- sistemaAvaliacaoArtigos/projeto/usuario/urls.py
+++ sistemaAvaliacaoArtigos/projeto/usuario/urls.py
@@ -7,6 +7,6 @@ from .views import UsuarioUpdateView, UsuarioDeleteView
 urlpatterns = [
 	path('list/$', UsuarioListView.as_view(), name='usuario_list'),
 	path('cad/$', UsuarioCreateView.as_view(), name='usuario_create'),
-	path('(?P<pk>\d+)/$', UsuarioUpdateView.as_view(), name='usuario_update'),
-	path('(?P<pk>\d+)/delete/$', UsuarioDeleteView.as_view(), name='usuario_delete'),
+	path('<slug:slug>/', UsuarioUpdateView.as_view(), name='usuario_update'),
+	path('<slug:slug>/delete/', UsuarioDeleteView.as_view(), name='usuario_delete'),
 ]
